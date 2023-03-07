aa0ba8f3a55e3fd7b087378e16bc38778f843a1f
Samya brata Maji <samyabratamaji334@gmail.com>
2023-03-08 00:44:53 +0530

    add: project section (#36)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import About from "./lib/about_section/about";
 import BlogSection from "./lib/blog_section/blog_section";
 import { Navbar } from "./lib/navbar/navbar";
 import { PROPS_NAVBAR } from "./lib/navbar/navbar.data";
+import ProjectSection from "./lib/project_section/project_section";
 
 function App() {
   return (
     <>
       <Navbar links={PROPS_NAVBAR} />
       <About />
-      <BlogSection/>
+      <BlogSection />
+      <ProjectSection />
     </>
   );
 }
---
+++ src/lib/project_section/project.data.ts
@@ -0,0 +1,25 @@
+import { ProjectData } from "./project.types";
+
+export const PROJECT_DATA: ProjectData[] = [
+  {
+    title: "LabForAll",
+    description:
+      "LabForAll is a virtual science lab that improves science comprehension via interactive experiments and simulations in physics, chemistry, and biology.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/116789799/222729093-bf73a510-2b0e-489f-a309-b6be128d7dc2.png",
+  },
+  {
+    title: "Magnificent Seven",
+    description:
+      "The Magnificent Seven offers an informative virtual experience of the world's awe-inspiring engineering marvels.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/116789799/222728893-22d95bd4-8392-4e67-8aa1-b87f4b294e52.png",
+  },
+  {
+    title: "Portfolio 2022",
+    description:
+      "The Magnificent Seven offers an informative virtual experience of the world's awe-inspiring engineering marvels.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/116789799/216760692-d46e4db1-3d73-4e39-808a-29ff91e36fbe.png",
+  },
+];
