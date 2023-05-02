8a1f0d8f0a6c5c513f938a8f982ff32eb9c3e730
Samya brata Maji <samyabratamaji334@gmail.com>
2023-05-02 17:46:36 +0530

    added hashnode cdn to remote pattern

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "cdn.hashnode.com",
+      port: "",
+      pathname: "/res/hashnode/image/upload/**",
+    }],
+  },
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     // Required:
