bfd3124e560e52512698f397713b79bf2fb8475e
Samya brata Maji <samyabratamaji334@gmail.com>
2023-05-06 12:50:50 +0530

    chore: added data fetching from github api

--- util/fetch.ts
+++ util/fetch.ts
@@ -1,5 +1,11 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
+export async function fetchGithubData(username: string) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if (response.ok) return response.json();
+    else throw new Error(response.status.toString());
+}
+
 export async function fetchHashnodeData(username: string): Promise<any> {
     const client = new ApolloClient({
       uri: "https://api.hashnode.com/",
