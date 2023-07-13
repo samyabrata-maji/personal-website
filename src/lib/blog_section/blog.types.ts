3356b1666c6468c4ff41a58a380b95cfe9d01707
Samya brata Maji <samyabratamaji334@gmail.com>
2023-07-13 21:03:31 +0530

    feat: rm src dir

--- src/lib/blog_section/blog.types.ts
+++
@@ -1,5 +0,0 @@
-export type BlogProps = {
-  contentMarkdown: string;
-  coverImage: string;
-  title: string;
-};
--- src/lib/footer/footer.types.ts
+++
@@ -1,4 +0,0 @@
-export interface FooterProps {
-  title: string;
-  links: { label: string; link: string }[];
-}
